02039829b8a01651df59b3fcdaec89c2f3ef80c9
Ujjawal Nehwal <ujjawalnehwal@gmail.com>
2026-07-22 02:20:56 +0530

    Added the Dev branch

---
+++ HalfAdder_GateLevel/HalfAdder_GateLevel.srcs/sources_1/new/HalfAdder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.07.2026 01:36:21
+// Design Name: 
+// Module Name: HalfAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module HalfAdder(
+
+    );
+endmodule
